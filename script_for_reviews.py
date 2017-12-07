f50329e64f72cc1d54eb467902af4f31e622f62f
Alex Clemens <aclemens18@cmc.edu>
2017-12-06 22:23:22 -0800

    Update script_for_reviews.py
    added 'drink'

--- script_for_reviews.py
+++ script_for_reviews.py
@@ -3,7 +3,7 @@ import yaml
 import requests
 
 
-keys = ['food', 'inside', 'outside', 'label', 'caption', 'food_and_caption', 'food_and_no_caption']
+keys = ['food', 'inside', 'outside', 'drink', 'label', 'caption', 'food_and_caption', 'food_and_no_caption']
 dictionary = dict.fromkeys(keys, 0)
 
 
@@ -25,8 +25,10 @@ for curr_string in curr:
     elif curr_string.get('label') == 'outside':
     	dictionary['outside'] += 1
     	dictionary['label'] += 1
+    elif curr_string.get('label') == 'drink':
+        dictionary['drink'] += 1
+        dictionary['label'] += 1
 
-        
     if curr_string.get('label') == 'food' and curr_string.get('caption') != "":
         dictionary['food_and_caption']+=1
     elif curr_string.get('label') == 'food' and curr_string.get('caption') == "":
@@ -37,6 +39,7 @@ print "reviews with a label: " + str(dictionary['label'])
 print "Label is food: " + str(dictionary['food'])
 print "Label is inside: " + str(dictionary['inside'])
 print "Label is outside: " + str(dictionary['outside'])
+print "Label is drink: " + str(dictionary['drink'])
 print "reviews with captions: " +  str(dictionary['caption'])
 print "food and caption " + str(dictionary['food_and_caption'])
 print "food and no caption " + str(dictionary['food_and_no_caption'])
