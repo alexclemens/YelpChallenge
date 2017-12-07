50557620db8ff21487f08e90c7d298b310e480f8
Alex Clemens <aclemens18@cmc.edu>
2017-12-06 19:17:00 -0800

    Update script_for_reviews.py

--- script_for_reviews.py
+++ script_for_reviews.py
@@ -3,12 +3,6 @@ import yaml
 import requests
 
 
-food = 0
-inside = 0
-outside = 0
-
-label = 0
-caption = 0
 keys = ['food', 'inside', 'outside', 'label', 'caption']
 dictionary = dict.fromkeys(keys, 0)
 
